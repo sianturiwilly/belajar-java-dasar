78778be5e30f22cfbe31338316808c9f54202128
sianturiwilly <prajudiwilliam10@gmail.com>
2022-10-26 13:46:22 +0700

    Commit keempat

--- src/main/java/org/example/Variable.java
+++ src/main/java/org/example/Variable.java
@@ -7,11 +7,20 @@ public class Variable {
         String name;
         name = "Willi Sianturi";
 
+        System.out.println(name);
+
         int age = 29;
         String address = "Indonesia";
 
-        System.out.println(name);
         System.out.println(age);
         System.out.println(address);
+
+        name = "Nike Ardilla";
+        System.out.println(name);
+
+        var firstname = "Willi";
+        var middleName = "Chrisdeardo";
+        var lastName = "Sianturi";
+
     }
 }
