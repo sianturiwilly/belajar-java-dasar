cf4f325f422967f452169d9f826b73ac7e2228dd
sianturiwilly <prajudiwilliam10@gmail.com>
2022-10-26 11:21:01 +0700

    Commit kedua

---
+++ src/main/java/org/example/TipeDataString.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class TipeDataString {
+
+    public static void main(String[] args) {
+
+        String firstname = "Willi";
+        String lastName = "Sianturi";
+        String fullname = firstname + " " + lastName;
+
+        System.out.println(firstname);
+        System.out.println(lastName);
+        System.out.println(fullname);
+    }
+}
